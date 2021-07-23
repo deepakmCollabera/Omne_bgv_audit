8dc5ff67bd94495297dc57ae0b5182b08ccac879
deepakmCollabera <61048374+deepakmCollabera@users.noreply.github.com>
2021-07-23 17:45:48 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,44 @@
+$(document).ready(function(){
+  $(".modal_open").click(function(e){
+    e.stopPropagation();
+    e.preventDefault();
+    let modal = $(this).attr("data-modalName");
+    $("#"+modal+"").addClass("active");
+  })
+
+  $(".custom_modal .close").click(function(){
+    // console.log("close");
+    $(this).closest(".custom_modal").removeClass("active");
+  })
+
+  // scroll_actions -active
+  $(".scroll_actions li span").click(function(){
+    $(this).parent().parent().parent(".scroll_actions").toggleClass("active");
+  })
+
+  // profile_card_show
+  $(".open_popup").click(function(e){
+    e.stopPropagation();
+    e.preventDefault();
+    $(this).children(".action_popup").toggleClass("active");
+  });
+
+  $(".close_popup").click(function(e){
+    e.stopPropagation();
+    e.preventDefault();
+    $(this).parent().parent(".action_popup").removeClass("active");
+  })
+  
+  $(".docs_mainwrap p, .info_table .accordion-toggle").click(function(e){
+    e.stopPropagation();
+    e.preventDefault();
+  })
+
+  // sidebar add class
+  $(".side_menu").click(function() {
+    $(".leftSideBar").toggleClass("active");
+  })
+})
+$(function() {
+  $('.box').matchHeight();
+});
